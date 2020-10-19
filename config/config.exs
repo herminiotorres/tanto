09c288debf8d13ef8511f2e72286bf184334fd3f
Herminio Torres <herminiocesar@gmail.com>
2020-10-19 15:30:42 -0300

    add a config for at least one repo and a config for my database where it use for my repo

--- config/config.exs
+++ config/config.exs
@@ -3,3 +3,13 @@ import Config
 # Ecto Repositories
 config :tanto,
   ecto_repos: [Tanto.Repo]
+  
+# Configure your database
+config :tanto, Tanto.Repo,
+  database: "tanto_repo_dev",
+  username: "postgres",
+  password: "postgres",
+  hostname: "localhost",
+  port: "5432",
+  show_sensitive_data_on_connection_error: true,
+  pool_size: 10
